e7eeebb417f0939fdb0ff456c1ee4d62ab9dcd07
pedronso <pedrocaetanoaraujopc@gmail.com>
2022-06-04 16:39:46 -0300

    add new functions

--- app/src/main/java/com/example/sceneform_maintained_test/MainActivity.java
+++ app/src/main/java/com/example/sceneform_maintained_test/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.widget.Switch;
 import android.widget.Toast;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +24,9 @@ import com.google.ar.core.Plane;
 import com.google.ar.core.Session;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.HitTestResult;
 import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.SceneView;
 import com.google.ar.sceneform.Sceneform;
 import com.google.ar.sceneform.math.Vector3;
@@ -38,11 +43,14 @@ public class MainActivity extends AppCompatActivity implements
         FragmentOnAttachListener,
         BaseArFragment.OnTapArPlaneListener,
         BaseArFragment.OnSessionConfigurationListener,
-        ArFragment.OnViewCreatedListener {
+        ArFragment.OnViewCreatedListener,
+        Node.OnTapListener{
 
     private ArFragment arFragment;
     private Renderable model;
-    private ViewRenderable viewRenderable;
+    private AnchorNode anchorNode;
+    TransformableNode modelNode;
+    private int qtd = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements
     public void loadModels() {
         WeakReference<MainActivity> weakActivity = new WeakReference<>(this);
         ModelRenderable.builder()
-                .setSource(this, R.raw.base_lamp_rig_first_low)
+                .setSource(this, R.raw.lamp_weapon_first_animation)
                 .setIsFilamentGltf(true)
                 .setAsyncLoadEnabled(true)
                 .build()
@@ -105,46 +113,60 @@ public class MainActivity extends AppCompatActivity implements
                             this, "Unable to load model", Toast.LENGTH_LONG).show();
                     return null;
                 });
-        ViewRenderable.builder()
-                .setView(this, R.layout.view_model_title)
-                .build()
-                .thenAccept(viewRenderable -> {
-                    MainActivity activity = weakActivity.get();
-                    if (activity != null) {
-                        activity.viewRenderable = viewRenderable;
-                    }
-                })
-                .exceptionally(throwable -> {
-                    Toast.makeText(this, "Unable to load model", Toast.LENGTH_LONG).show();
-                    return null;
-                });
     }
 
     @Override
     public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
-        if (model == null || viewRenderable == null) {
+        if (model == null) {
             Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
             return;
         }
+        if(qtd<1) {
+            Toast.makeText(this, "Loaded", Toast.LENGTH_SHORT).show();
+            // Create the Anchor.
+            Anchor anchor = hitResult.createAnchor();
+            anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
 
-        // Create the Anchor.
-        Anchor anchor = hitResult.createAnchor();
-        AnchorNode anchorNode = new AnchorNode(anchor);
-        anchorNode.setParent(arFragment.getArSceneView().getScene());
+            // Create the transformable model and add it to the anchor.
+            modelNode = new TransformableNode(arFragment.getTransformationSystem());
 
-        // Create the transformable model and add it to the anchor.
-        TransformableNode model = new TransformableNode(arFragment.getTransformationSystem());
-        model.setParent(anchorNode);
-        model.setRenderable(this.model)
-                .animate(true).start();
-        model.select();
-        model.setLocalScale(new Vector3(1f, 1f, 1f));
+            //model.getScaleController().setMinScale(0.1f);
 
-        Node titleNode = new Node();
-        titleNode.setParent(model);
-        titleNode.setEnabled(false);
-        titleNode.setLocalPosition(new Vector3(0.0f, 1.0f, 0.0f));
-        titleNode.setRenderable(viewRenderable);
-        titleNode.setEnabled(true);
+            modelNode.setLocalScale(new Vector3(1f, 1f, 1f));
+            Log.i("teste", "teste" + modelNode.getLocalScale());
+
+            modelNode.setParent(anchorNode);
+
+            modelNode.setRenderable(this.model)
+                    .animate(false);
+
+            modelNode.select();
+            qtd++;
+        }else{
+            Toast.makeText(this, "Max reached", Toast.LENGTH_SHORT).show();
+        }
+
     }
+
+    @Override
+    public void onTap(HitTestResult hitTestResult, MotionEvent motionEvent) {
+            Toast.makeText(this, "Touch tapped", Toast.LENGTH_SHORT).show();
+    }
+
+    public void removeNode(View v){
+        Toast.makeText(this, "Removed", Toast.LENGTH_SHORT).show();
+         modelNode.setParent(null);
+         this.qtd--;
+    }
+    public void hideShow(View v){
+        Toast.makeText(this, "Enable/Disable", Toast.LENGTH_SHORT).show();
+        modelNode.setEnabled(!modelNode.isEnabled());
+    }
+    public void modelAnimation(View v){
+        Toast.makeText(this, "Animation", Toast.LENGTH_SHORT).show();
+        modelNode.setRenderable(this.model)
+                .animate(false).start();
+    }
+
 }
